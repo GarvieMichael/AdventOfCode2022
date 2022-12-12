0a11381c75c4137b50ee5cb746ea41f1d80a1b44
Michael Garvie <mmgarvie@gmail.com>
2022-12-12 10:53:43 +0200

    Day 5 init

---
+++ Day5/Day5.py
@@ -0,0 +1,5 @@
+# /usr/bin/env /bin/python3 /home/michaelgarvie/.vscode/extensions/ms-python.python-2022.8.1/pythonFiles/lib/python/debugpy/launcher 42419 -- /home/michaelgarvie/Documents/Work_Documentation/AdventOfCode2022/Day1/Day1.py
+text_file = open("Day5/example.txt")
+crates = text_file.readlines()
+
+print(crates)
